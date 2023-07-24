51281ac60d361373372b2705e4670437e9262b9e
Xalilillo2008/Xalilillo2008 <nasibahon837@gmail.com>
2023-07-24 21:53:10 +0300

    inital commit

---
+++ dars-1/js/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+let num = prompt('raqam kiriting')
+
+
+    if(num % 3 === 0 && num % 7 === 0) {
+        console.log ("3 va 7 bo'lindi");
+    }else if(num % 3 === 0) {
+        console.log ("3 ga bo'lindi");
+    } else if(num % 7 === 0) {
+        console.log ("7 ga bo'lindi");
+    } 
+    
